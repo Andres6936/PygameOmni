decca26714537f5a7732ccbcf70cf58b7e167c02
Andres6936 <andres6936@live.com>
2017-06-09 16:35:25 -0500

    Update EnumConstantes.py

--- Source/Enum/EnumConstantes.py
+++ Source/Enum/EnumConstantes.py
@@ -6,5 +6,36 @@ from enum import Enum
 class EnumConstantes( Enum ):
 
     MAPA_ALTO: int = 512
+    """
+    Alto del mapa en pixeles.
+    """
+
     MAPA_ANCHO: int = 1024
+    """
+    Ancho del mapa en pixeles.
+    """
+
     PIXELES: int = 16
+    """
+    Alto y ancho de cada sprite en pixeles.
+    """
+
+    MAXIMO_ARMADURA_NIVEL: int = 3
+    """
+    Número máximo de armaduras por nivel.
+    """
+
+    MAXIMO_ESPADA_NIVEL: int = 3
+    """
+    Número máximo de espadas por nivel.
+    """
+
+    MAXIMO_POCION_NIVEL: int = 4
+    """
+    Número máximo de pociones por nivel.
+    """
+
+    MAXIMO_ENEMIGOS_NIVEL: int = 15
+    """
+    Máximo de enemigos que pueden haber en un nivel del mapa.
+    """
